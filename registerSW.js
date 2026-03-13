3a8ad877093b48059b9a56fd92194c1b345a0699
mrnagydavid <mrnagydavid@users.noreply.github.com>
2026-03-13 15:40:52 +0000

    deploy: 603fcb01d2e3763ec2ed5739fff72fcf009152c5

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/calorie-counter/sw.js', { scope: '/calorie-counter/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
